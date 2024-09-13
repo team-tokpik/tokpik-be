47ca3f66c582df19cb19efbd5dd57b5f5c6e11d5
Minjae-An <mj3242@naver.com>
2024-09-13 15:46:07 +0900

    chore: 프로젝트 초기 설정(#1)

---
+++ src/main/java/org/example/tokpik_be/TokpikBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.tokpik_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokpikBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokpikBeApplication.class, args);
+    }
+
+}
