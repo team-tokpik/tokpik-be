095933dcbd34e65c69a050ae0f563fd137f468e4
hyunji <phj9573@naver.com>
2024-09-18 14:47:54 +0900

    feat: 사용자 대화 토픽 태그 엔티티 정의(#10)

---
+++ src/main/java/org/example/tokpik_be/tag/entity/UserTopicTag.java
@@ -0,0 +1,42 @@
+package org.example.tokpik_be.tag.entity;
+
+import org.example.tokpik_be.common.BaseTimeEntity;
+import org.example.tokpik_be.tag.domain.TopicTag;
+import org.example.tokpik_be.user.domain.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "user_talk_topic_tags")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserTopicTag extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_tag_id", nullable = false)
+    private TopicTag topicTag;
+
+    public UserTopicTag(User user, TopicTag topicTag) {
+        this.user = user;
+        this.topicTag = topicTag;
+    }
+
+}
