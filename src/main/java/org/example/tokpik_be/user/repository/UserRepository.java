a3c12732a7638e5982cdd4b8eee56206b8cfe595
Minjae-An <mj3242@naver.com>
2024-09-18 18:38:08 +0900

    feat: 이메일 기반 사용자 조회 로직 추가(#5)
    - 이메일 기반 사용자 존재 여부 조회
    - 이메일 기반 사용자 조회

--- src/main/java/org/example/tokpik_be/user/repository/UserRepository.java
+++ src/main/java/org/example/tokpik_be/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.example.tokpik_be.user.repository;
 
+import java.util.Optional;
 import org.example.tokpik_be.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
