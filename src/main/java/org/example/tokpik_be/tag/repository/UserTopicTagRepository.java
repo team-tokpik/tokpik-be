6ae45cdf9dff8520cd5fbf6bed7c1f6c70c17143
hyunji <phj9573@naver.com>
2024-09-18 14:49:27 +0900

    feat: UserTopicTagRepository 정의(#10)

---
+++ src/main/java/org/example/tokpik_be/tag/repository/UserTopicTagRepository.java
@@ -0,0 +1,8 @@
+package org.example.tokpik_be.tag.repository;
+
+import org.example.tokpik_be.tag.entity.UserTopicTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTopicTagRepository extends JpaRepository<UserTopicTag, Long> {
+
+}
