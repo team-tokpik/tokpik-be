7d6fb02380a69bbbeee4c8066c03dde3ad7a94f0
Minjae-An <mj3242@naver.com>
2024-09-18 19:03:20 +0900

    feat: refresh token 갱신 로직 추가(#5)

--- src/main/java/org/example/tokpik_be/user/domain/User.java
+++ src/main/java/org/example/tokpik_be/user/domain/User.java
@@ -52,4 +52,8 @@ public class User extends BaseTimeEntity {
         this.birth = birth;
         this.gender = gender;
     }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
