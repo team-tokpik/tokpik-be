adc3c582931e16be18d348edca381283968e13c3
Minjae-An <mj3242@naver.com>
2024-09-19 17:01:53 +0900

    feat: Gender null인 경우 대한 convert 처리 추가(#5)
    신규 가입한 사용자의 경우 성별이 null이므로 변환시 고려하려 처리하도록 로직 수정

--- src/main/java/org/example/tokpik_be/user/converter/GenderConverter.java
+++ src/main/java/org/example/tokpik_be/user/converter/GenderConverter.java
@@ -2,6 +2,7 @@ package org.example.tokpik_be.user.converter;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import java.util.Optional;
 import org.example.tokpik_be.user.enums.Gender;
 
 @Converter
@@ -10,12 +11,12 @@ public class GenderConverter implements AttributeConverter<Gender, Boolean> {
     @Override
     public Boolean convertToDatabaseColumn(Gender gender) {
 
-        return gender.toBoolean();
+        return Optional.ofNullable(gender).map(Gender::toBoolean).orElse(null);
     }
 
     @Override
     public Gender convertToEntityAttribute(Boolean dbData) {
 
-        return Gender.from(dbData);
+        return Optional.ofNullable(dbData).map(Gender::from).orElse(null);
     }
 }
