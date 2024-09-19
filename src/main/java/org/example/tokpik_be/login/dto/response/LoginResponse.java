f445bff9d8f97c0753d0df07666d40ebdf8009e5
Minjae-An <mj3242@naver.com>
2024-09-19 17:00:16 +0900

    feat: 프로필 생성 필요 여부 필드 추가(#5)
    소셜 로그인을 통해 신규 가입 처리된 회원의 경우 프로필 생성 필요.
    해당 정보를 제공하기 위한 필드 추가

--- src/main/java/org/example/tokpik_be/login/dto/response/LoginResponse.java
+++ src/main/java/org/example/tokpik_be/login/dto/response/LoginResponse.java
@@ -3,10 +3,15 @@ package org.example.tokpik_be.login.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginResponse(
+    @Schema(type = "boolean", description = "프로필 생성 필요 여부, true일 경우 필요", example = "true")
+    boolean requiresProfile,
+
     @Schema(type = "string", description = "access token", example = "header.payload.signature")
     String accessToken,
+
     @Schema(type = "string", description = "refresh token", example = "header.payload.signature")
     String refreshToken
 ) {
 
 }
+
