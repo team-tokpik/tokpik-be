96bb4c01c9467851d232b70be8a07b6fcc0c4e96
Minjae-An <mj3242@naver.com>
2024-09-19 16:59:14 +0900

    feat: 사용하지 않는 예외 삭제(#5)

--- src/main/java/org/example/tokpik_be/exception/LoginException.java
+++ src/main/java/org/example/tokpik_be/exception/LoginException.java
@@ -8,8 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum LoginException implements BaseException {
 
-    LOGIN_FAIL(HttpStatus.BAD_REQUEST, "로그인 과정중 에러 발생, 재로그인 필요"),
-    JOIN_REQUIRED(HttpStatus.BAD_REQUEST, "미가입 사용자, 가입 과정 필요");
+    LOGIN_FAIL(HttpStatus.BAD_REQUEST, "로그인 과정중 에러 발생, 재로그인 필요");
 
     private final HttpStatus status;
     private final String message;
