5aa1c379e52c18fe541093126662cd1fc1db41c2
Minjae-An <mj3242@naver.com>
2024-09-19 17:05:09 +0900

    feat: 프로필(부가 정보) 생성 API 추가(#5)
    신규 가입 사용자의 부가 정보 생성을 위한 API 구현

--- src/main/java/org/example/tokpik_be/user/controller/UserController.java
+++ src/main/java/org/example/tokpik_be/user/controller/UserController.java
@@ -2,19 +2,37 @@ package org.example.tokpik_be.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.tokpik_be.user.dto.request.UserMakeProfileRequest;
 import org.example.tokpik_be.user.dto.response.UserProfileResponse;
+import org.example.tokpik_be.user.service.UserCommandService;
 import org.example.tokpik_be.user.service.UserQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
+
+    @Operation(summary = "프로필(부가 정보) 생성", description = "사용자 부가 정보 입력")
+    @ApiResponse(responseCode = "200", description = "프로필 생성 성공")
+    @PostMapping("/users/profiles")
+    public ResponseEntity<Void> makeProfile(@RequestAttribute("userId") long userId,
+        @RequestBody @Valid UserMakeProfileRequest request) {
+
+        userCommandService.makeProfile(userId, request);
+
+        return ResponseEntity.ok().build();
+    }
 
     @Operation(summary = "마이 페이지 프로필 조회", description = "마이 페이지 사용자 프로필 조회")
     @ApiResponse(responseCode = "200", description = "프로필 조회 성공")
