33d388430b0e1297965857fd6297c7c9484ae619
Minjae-An <mj3242@naver.com>
2024-09-20 10:18:20 +0900

    feat: gender 필드 boolean 타입으로 변경, 생년월일 검증 추가(#5)
    - gender 필드 boolean 타입으로 변경
    - 생년월일 과거,현재만 가능토록 검증 추가

--- src/main/java/org/example/tokpik_be/user/dto/request/UserMakeProfileRequest.java
+++ src/main/java/org/example/tokpik_be/user/dto/request/UserMakeProfileRequest.java
@@ -1,20 +1,18 @@
 package org.example.tokpik_be.user.dto.request;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
-import org.example.tokpik_be.user.deserializer.GenderDeserializer;
-import org.example.tokpik_be.user.enums.Gender;
 
 public record UserMakeProfileRequest(
     @Schema(type = "string", description = "생년월일", example = "1998-04-08")
     @NotNull(message = "생년월일은 필수값")
+    @PastOrPresent(message = "생년월일은 과거,현재만 가능")
     LocalDate birth,
 
     @Schema(type = "boolean", description = "성별(남=true, 여=false)", example = "true")
-    @JsonDeserialize(using = GenderDeserializer.class)
-    Gender gender
+    boolean gender
 ) {
 
 }
