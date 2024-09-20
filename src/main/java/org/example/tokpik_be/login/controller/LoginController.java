91ebda9ec07ab604a78656a268c7b7419eef2a43
Minjae-An <mj3242@naver.com>
2024-09-20 17:26:42 +0900

    feat: access token refresh API 구현(#14)

--- src/main/java/org/example/tokpik_be/login/controller/LoginController.java
+++ src/main/java/org/example/tokpik_be/login/controller/LoginController.java
@@ -1,16 +1,23 @@
 package org.example.tokpik_be.login.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.LoginException;
+import org.example.tokpik_be.login.dto.request.AccessTokenRefreshRequest;
+import org.example.tokpik_be.login.dto.response.AccessTokenRefreshResponse;
 import org.example.tokpik_be.login.dto.response.LoginResponse;
 import org.example.tokpik_be.login.service.LoginCommandService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Login API", description = "로그인 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
@@ -27,6 +34,15 @@ public class LoginController {
         }
 
         LoginResponse response = loginCommandService.kakaoLogin(code);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AccessTokenRefreshResponse> refreshAccessToken(
+        @RequestBody @Valid AccessTokenRefreshRequest request) {
+
+        AccessTokenRefreshResponse response = loginCommandService.refreshAccessToken(request);
 
         return ResponseEntity.ok().body(response);
     }
