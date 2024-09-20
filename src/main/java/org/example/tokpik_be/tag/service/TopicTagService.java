91f2cd8d968d070bf017a8cdbdd26e57e27e44ee
hyunji <phj9573@naver.com>
2024-09-20 13:30:47 +0900

    feat: 사용자 예외 처리 로직 추가(#10)

--- src/main/java/org/example/tokpik_be/tag/service/TopicTagService.java
+++ src/main/java/org/example/tokpik_be/tag/service/TopicTagService.java
@@ -3,22 +3,32 @@ package org.example.tokpik_be.tag.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.example.tokpik_be.exception.GeneralException;
+import org.example.tokpik_be.exception.UserException;
 import org.example.tokpik_be.tag.domain.TopicTag;
 import org.example.tokpik_be.tag.dto.response.UserTopicTagResponse;
 import org.example.tokpik_be.tag.entity.UserTopicTag;
 import org.example.tokpik_be.tag.repository.UserTopicTagRepository;
+import org.example.tokpik_be.user.domain.User;
+import org.example.tokpik_be.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class TopicTagService {
 
-    @Autowired
-    private UserTopicTagRepository userTopicTagRepository;
+    private final UserTopicTagRepository userTopicTagRepository;
+
+    private final UserRepository userRepository;
 
     public UserTopicTagResponse getUserTopicTags(long userId) {
 
-        List<UserTopicTag> userTopicTags = userTopicTagRepository.findByUserId(userId);
+        User user = findById(userId);
+
+        List<UserTopicTag> userTopicTags = userTopicTagRepository.findByUserId(user.getId());
 
         List<UserTopicTagResponse.TopicTagDTO> topicTagDTOList = userTopicTags.stream()
             .map(userTopicTag -> {
@@ -31,5 +41,10 @@ public class TopicTagService {
             .collect(Collectors.toList());
 
         return new UserTopicTagResponse(userId, topicTagDTOList);
+    }
+
+    private User findById(long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
     }
 }
