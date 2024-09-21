1bc733b8dd66f8481572387b378c5e268ed8d691
Minjae-An <mj3242@naver.com>
2024-09-21 17:26:55 +0900

    feat: 불필요한 refresh token 필드 삭제(#14)

--- src/main/java/org/example/tokpik_be/login/dto/response/AccessTokenRefreshResponse.java
+++ src/main/java/org/example/tokpik_be/login/dto/response/AccessTokenRefreshResponse.java
@@ -1,8 +1,10 @@
 package org.example.tokpik_be.login.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AccessTokenRefreshResponse(
-    String accessToken,
-    String refreshToken
+    @Schema(type = "string", description = "새로운 access token", example = "header.payload.signature")
+    String accessToken
 ) {
 
 }
