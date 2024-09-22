89074199deed80cb3f47c35d93631d57352f776a
Minjae-An <mj3242@naver.com>
2024-09-22 17:08:44 +0900

    feat: 사용자 대화 장소 태그 엔티티 추가(#12)
    해당 엔티티에서 사용자로의 접근은 사용되지 않기에 기본 타입 userId 필드로
    관계 표현

---
+++ src/main/java/org/example/tokpik_be/tag/entity/UserPlaceTag.java
@@ -0,0 +1,36 @@
+package org.example.tokpik_be.tag.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tokpik_be.common.BaseTimeEntity;
+import org.example.tokpik_be.tag.domain.PlaceTag;
+
+@Table(name = "user_place_tags")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserPlaceTag extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "talk_place_tag_id")
+    private PlaceTag placeTag;
+
+    public UserPlaceTag(long userId, PlaceTag placeTag) {
+        this.userId = userId;
+        this.placeTag = placeTag;
+    }
+}
