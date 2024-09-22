5ec09ef911fbf6be16d59ecff6d7f979473705bd
Minjae-An <mj3242@naver.com>
2024-09-22 17:13:19 +0900

    feat: 대화 주제 엔티티 정의(#12)

---
+++ src/main/java/org/example/tokpik_be/talk_topic/domain/TalkTopic.java
@@ -0,0 +1,62 @@
+package org.example.tokpik_be.talk_topic.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tokpik_be.common.BaseTimeEntity;
+import org.example.tokpik_be.tag.domain.PlaceTag;
+import org.example.tokpik_be.tag.domain.TopicTag;
+
+@Table(name = "talk_topics")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TalkTopic extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String subtitle;
+
+    @Column(nullable = false)
+    private String situation;
+
+    @Embedded
+    private TalkPartner partner;
+
+    @OneToOne
+    @JoinColumn(name = "talk_topic_tag_id")
+    private TopicTag topicTag;
+
+    @OneToOne
+    @JoinColumn(name = "talk_place_tag_id")
+    private PlaceTag placeTag;
+
+    public TalkTopic(String title,
+        String subtitle,
+        String situation,
+        TalkPartner partner,
+        TopicTag topicTag,
+        PlaceTag placeTag) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.situation = situation;
+        this.partner = partner;
+        this.topicTag = topicTag;
+        this.placeTag = placeTag;
+    }
+}
