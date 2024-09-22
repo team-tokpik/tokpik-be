e1489cd83f4942935ea0ea3ef8d77bbdc0e9d33c
Minjae-An <mj3242@naver.com>
2024-09-22 17:11:48 +0900

    feat: 대화 상대(파트너) 타입 정의(#12)
    대화 상대와 관련한 필드들, 묶어서 표현하기 위한 임베딩 타입 정의

---
+++ src/main/java/org/example/tokpik_be/talk_topic/domain/TalkPartner.java
@@ -0,0 +1,28 @@
+package org.example.tokpik_be.talk_topic.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.tokpik_be.user.converter.GenderConverter;
+import org.example.tokpik_be.user.enums.Gender;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TalkPartner {
+
+    @Column(name = "partner_gender", nullable = false)
+    @Convert(converter = GenderConverter.class)
+    private Gender gender;
+
+    @Column(name = "partner_age_lower_bound", nullable = false)
+    private int ageLowerBound;
+
+    @Column(name = "partner_age_upper_bound", nullable = false)
+    private int ageUpperBound;
+}
