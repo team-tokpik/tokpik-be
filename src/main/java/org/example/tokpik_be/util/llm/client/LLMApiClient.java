ba36c1535739266dc9dccad862bb05c1df300a6c
Minjae-An <mj3242@naver.com>
2024-09-25 21:05:27 +0900

    feat: LLM 응답 DTO 별도 정의에 따른 수정(#26)

--- src/main/java/org/example/tokpik_be/util/llm/client/LLMApiClient.java
+++ src/main/java/org/example/tokpik_be/util/llm/client/LLMApiClient.java
@@ -1,10 +1,9 @@
 package org.example.tokpik_be.util.llm.client;
 
-import java.util.List;
-import org.example.tokpik_be.talk_topic.dto.response.TalkTopicSearchResponse;
 import org.example.tokpik_be.util.llm.dto.request.LLMTalkTopicSearchRequest;
+import org.example.tokpik_be.util.llm.dto.response.LLMTalkTopicsResponse;
 
 public interface LLMApiClient {
 
-    List<TalkTopicSearchResponse> searchTalkTopics(LLMTalkTopicSearchRequest request);
+    LLMTalkTopicsResponse searchTalkTopics(LLMTalkTopicSearchRequest request);
 }
