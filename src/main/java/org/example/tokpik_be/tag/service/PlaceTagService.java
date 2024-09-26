f59881a59373dcda0f40a386754aa908855c0e47
Minjae-An <mj3242@naver.com>
2024-09-26 13:50:50 +0900

    feat: 전체 대화 장소 조회 로직 추가(#33)

--- src/main/java/org/example/tokpik_be/tag/service/PlaceTagService.java
+++ src/main/java/org/example/tokpik_be/tag/service/PlaceTagService.java
@@ -6,6 +6,8 @@ import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.TagException;
 import org.example.tokpik_be.tag.domain.PlaceTag;
 import org.example.tokpik_be.tag.dto.request.UserPlaceTagsRequest;
+import org.example.tokpik_be.tag.dto.response.PlaceTagTotalResponse;
+import org.example.tokpik_be.tag.dto.response.PlaceTagTotalResponse.PlaceTagResponse;
 import org.example.tokpik_be.tag.dto.response.UserPlaceTagResponse;
 import org.example.tokpik_be.tag.entity.UserPlaceTag;
 import org.example.tokpik_be.tag.repository.PlaceTagRepository;
@@ -57,15 +59,25 @@ public class PlaceTagService {
             if (!placeTagRepository.existsById(tagId)) {
                 throw new GeneralException(TagException.TAG_NOT_FOUND);
             }
-            UserPlaceTag userPlaceTag = new UserPlaceTag(user.getId(), placeTagRepository.findById(tagId).get());
+            UserPlaceTag userPlaceTag = new UserPlaceTag(user.getId(),
+                placeTagRepository.findById(tagId).get());
             userPlaceTagRepository.save(userPlaceTag);
         }
 
         List<UserPlaceTag> updatedTags = userPlaceTagRepository.findByUserId(user.getId());
         List<UserPlaceTagResponse.PlaceTagDTO> placeTagDTOList = updatedTags.stream()
-            .map(tag -> new UserPlaceTagResponse.PlaceTagDTO(tag.getPlaceTag().getId(), tag.getPlaceTag().getContent()))
+            .map(tag -> new UserPlaceTagResponse.PlaceTagDTO(tag.getPlaceTag().getId(),
+                tag.getPlaceTag().getContent()))
             .toList();
 
         return new UserPlaceTagResponse(userId, placeTagDTOList);
+    }
+
+    @Transactional(readOnly = true)
+    public PlaceTagTotalResponse getAllPlaceTags() {
+        List<PlaceTag> placeTags = placeTagRepository.findAll();
+        List<PlaceTagResponse> responses = placeTags.stream().map(PlaceTagResponse::from).toList();
+
+        return new PlaceTagTotalResponse(responses);
     }
 }
