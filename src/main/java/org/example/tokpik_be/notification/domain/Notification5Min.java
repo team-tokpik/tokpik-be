77950b4dea93990a5e808f4b7fc7aec96caee850
Minjae-An <mj3242@naver.com>
2024-09-26 21:19:09 +0900

    feat: 5분 간격 알림 엔티티 정의(#39)

---
+++ src/main/java/org/example/tokpik_be/notification/domain/Notification5Min.java
@@ -0,0 +1,10 @@
+package org.example.tokpik_be.notification.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "notifications_5min")
+@Entity
+public class Notification5Min extends BaseNotification {
+
+}
