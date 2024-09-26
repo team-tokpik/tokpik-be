dc3b0e083e91ba0fbb850793d505a86f599f4159
Minjae-An <mj3242@naver.com>
2024-09-26 15:35:18 +0900

    feat: 프로필 생성시 사용자 대화 태그 갱신토록 로직 수정(#33)

--- src/main/java/org/example/tokpik_be/user/service/UserCommandService.java
+++ src/main/java/org/example/tokpik_be/user/service/UserCommandService.java
@@ -1,6 +1,16 @@
 package org.example.tokpik_be.user.service;
 
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.example.tokpik_be.tag.domain.PlaceTag;
+import org.example.tokpik_be.tag.domain.TopicTag;
+import org.example.tokpik_be.tag.entity.UserPlaceTag;
+import org.example.tokpik_be.tag.entity.UserTopicTag;
+import org.example.tokpik_be.tag.repository.PlaceTagRepository;
+import org.example.tokpik_be.tag.repository.TopicTagRepository;
+import org.example.tokpik_be.tag.repository.UserPlaceTagRepository;
+import org.example.tokpik_be.tag.repository.UserTopicTagRepository;
 import org.example.tokpik_be.user.domain.User;
 import org.example.tokpik_be.user.dto.request.UserMakeProfileRequest;
 import org.example.tokpik_be.user.enums.Gender;
@@ -13,7 +23,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserCommandService {
 
+    private final TopicTagRepository topicTagRepository;
+    private final PlaceTagRepository placeTagRepository;
     private final UserRepository userRepository;
+    private final UserTopicTagRepository userTopicTagRepository;
+    private final UserPlaceTagRepository userPlaceTagRepository;
+
     private final UserQueryService userQueryService;
 
     public User save(User user) {
@@ -25,5 +40,50 @@ public class UserCommandService {
         User user = userQueryService.findById(userId);
         Gender gender = Gender.from(request.gender());
         user.updateProfile(request.birth(), gender);
+
+        updateUserTopicTags(user, request.topicTagIds());
+        updateUserPlaceTags(user, request.placeTagIds());
+    }
+
+    private void updateUserTopicTags(User user, List<Long> topicTagIds) {
+        if (Objects.isNull(topicTagIds) || topicTagIds.isEmpty()) {
+
+            return;
+        }
+
+        long userId = user.getId();
+        List<TopicTag> topicTags = topicTagRepository.findAllById(topicTagIds);
+        if (topicTags.isEmpty()) {
+
+            return;
+        }
+
+        List<UserTopicTag> userTopicTags = topicTags.stream()
+            .map(topicTag -> new UserTopicTag(userId, topicTag))
+            .toList();
+        userTopicTagRepository.deleteByUserId(userId);
+        userTopicTagRepository.saveAll(userTopicTags);
+        user.updateUserTopicTags(userTopicTags);
+    }
+
+    private void updateUserPlaceTags(User user, List<Long> placeTagIds) {
+        if (Objects.isNull(placeTagIds) || placeTagIds.isEmpty()) {
+
+            return;
+        }
+
+        long userId = user.getId();
+        List<PlaceTag> placeTags = placeTagRepository.findAllById(placeTagIds);
+        if (placeTags.isEmpty()) {
+
+            return;
+        }
+
+        List<UserPlaceTag> userPlaceTags = placeTags.stream()
+            .map(placeTag -> new UserPlaceTag(userId, placeTag))
+            .toList();
+        userPlaceTagRepository.deleteByUserId(userId);
+        userPlaceTagRepository.saveAll(userPlaceTags);
+        user.updateUserPlaceTags(userPlaceTags);
     }
 }
