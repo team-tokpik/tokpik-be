2e6ba15818f8a3fed2aab8c0cdf2086c2ab0019f
Minjae-An <mj3242@naver.com>
2024-09-27 17:34:56 +0900

    feat: batch deletion 스케줄링 로직 추가(#39)
    - 3개월에 한 번 말일 자정에 알림 batch deletion 수행
    - 데이터 추세에 따라 작업 수행 기간 조정 가능

--- src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
+++ src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
@@ -2,7 +2,9 @@ package org.example.tokpik_be.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.notification.dto.request.NotificationDeleteRequest;
+import org.example.tokpik_be.notification.repository.NotificationTopicRepository;
 import org.example.tokpik_be.notification.strategy.command.NotificationCommandStrategyFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class NotificationCommandService {
 
+    private final NotificationTopicRepository notificationTopicRepository;
     private final NotificationCommandStrategyFactory notificationCommandStrategyFactory;
 
     public void deleteNotification(NotificationDeleteRequest request) {
 
-        notificationCommandStrategyFactory
-            .deleteByIdAndIntervalMinutes(request.notificationId(),
+        notificationCommandStrategyFactory.deleteByIdAndIntervalMinutes(request.notificationId(),
                 request.notificationIntervalMinutes());
+    }
+
+    @Scheduled(cron = "0 0 0 L 3,6,9,12 ?")
+    public void deleteNotifications() {
+        notificationCommandStrategyFactory.deleteNotificationsInBatch();
     }
 }
