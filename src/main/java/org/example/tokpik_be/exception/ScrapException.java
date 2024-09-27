d586ec6d9cf63fdeb297ac7b813f242f4acdad46
박현지 <119161420+hhyuun@users.noreply.github.com>
2024-09-27 22:57:02 +0900

    대화 주제 스크랩 취소 기능 개발 (#49)
    * feat: 대화 주제 스크랩 취소 로직 구현(#48)
    
    * feat: 대화 주제 스크랩 취소 API 추가(#48)

--- src/main/java/org/example/tokpik_be/exception/ScrapException.java
+++ src/main/java/org/example/tokpik_be/exception/ScrapException.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ScrapException implements BaseException {
 
-    SCRAP_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 스크랩");
+    SCRAP_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 스크랩"),
+    SCRAP_TOPIC_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 스크랩 대화 주제"),
+    INVALID_SCRAP_TOPIC(HttpStatus.BAD_REQUEST, "해당 스크랩에 저장되지 않은 스크랩 대화 주제");
 
     private final HttpStatus status;
     private final String message;
