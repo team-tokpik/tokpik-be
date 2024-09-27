f78b93e872cf7fc108411fd002c38dc12aa23e02
Minjae-An <mj3242@naver.com>
2024-09-27 21:28:29 +0900

    feat: 삭제된 알림 제거 스케줄러 추가(#39)
    - 3개월마다 자정에 삭제된 알림 제거 작업 수행
    - ID를 기반으로 제거 작업 수행, 엔티티 기반 삭제 보다 처리 속도 향상

--- src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
+++ src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
@@ -1,11 +1,16 @@
 package org.example.tokpik_be.notification.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.NotificationException;
 import org.example.tokpik_be.notification.domain.Notification;
+import org.example.tokpik_be.notification.domain.NotificationTalkTopic;
+import org.example.tokpik_be.notification.repository.NotificationRepository;
+import org.example.tokpik_be.notification.repository.NotificationTalkTopicRepository;
 import org.example.tokpik_be.user.domain.User;
 import org.example.tokpik_be.user.service.UserQueryService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class NotificationCommandService {
+
+    private final NotificationRepository notificationRepository;
+    private final NotificationTalkTopicRepository notificationTalkTopicRepository;
 
     private final NotificationQueryService notificationQueryService;
     private final UserQueryService userQueryService;
@@ -26,5 +34,19 @@ public class NotificationCommandService {
         }
 
         notification.delete();
+    }
+
+    @Scheduled(cron = "0 0 0 L 3,6,9,12 ?")
+    public void deleteNotifications() {
+        List<Notification> notifications = notificationRepository.findAllByDeletedIsTrue();
+        List<Long> notificationIds = notifications.stream().map(Notification::getId).toList();
+        notificationRepository.deleteAllByIdInBatch(notificationIds);
+
+        List<NotificationTalkTopic> notificationTalkTopics = notificationTalkTopicRepository
+            .findAllByNotificationIdIn(notificationIds);
+        List<Long> notificationTalkTopicIds = notificationTalkTopics.stream()
+            .map(NotificationTalkTopic::getId)
+            .toList();
+        notificationTalkTopicRepository.deleteAllByIdInBatch(notificationTalkTopicIds);
     }
 }
