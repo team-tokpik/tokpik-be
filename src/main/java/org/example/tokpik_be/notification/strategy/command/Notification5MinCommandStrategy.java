77966ac6d52032d658ed30d71854a0cb517bc8a8
Minjae-An <mj3242@naver.com>
2024-09-27 17:31:23 +0900

    feat: 5분 간격 알림 batch deletion 로직 구현(#39)

--- src/main/java/org/example/tokpik_be/notification/strategy/command/Notification5MinCommandStrategy.java
+++ src/main/java/org/example/tokpik_be/notification/strategy/command/Notification5MinCommandStrategy.java
@@ -1,7 +1,11 @@
 package org.example.tokpik_be.notification.strategy.command;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.tokpik_be.notification.domain.BaseNotification;
+import org.example.tokpik_be.notification.domain.Notification5Min;
 import org.example.tokpik_be.notification.repository.Notification5MinRepository;
+import org.example.tokpik_be.notification.strategy.query.Notification5MinQueryStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,11 +13,19 @@ import org.springframework.stereotype.Component;
 public class Notification5MinCommandStrategy implements NotificationCommandStrategy {
 
     private final Notification5MinRepository notification5MinRepository;
+    private final Notification5MinQueryStrategy notification5MinQueryStrategy;
 
     @Override
     public void deleteById(long notificationId) {
+        BaseNotification notification = notification5MinQueryStrategy.findById(notificationId);
+        notification.delete();
+    }
 
-        notification5MinRepository.deleteById(notificationId);
+    @Override
+    public void deleteAllInBatch() {
+        List<Notification5Min> notifications = notification5MinRepository.findAllByDeletedIsTrue();
+        List<Long> notificationIds = notifications.stream().map(Notification5Min::getId).toList();
+        notification5MinRepository.deleteAllByIdInBatch(notificationIds);
     }
 
     @Override
