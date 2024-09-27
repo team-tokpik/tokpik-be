436c8ce07da00a2fd478c2fbabd3061766c241ba
Minjae An <101340860+Minjae-An@users.noreply.github.com>
2024-09-27 16:28:39 +0900

    연관 대화 주제 조회 기능 개발 (#43)
    * chore: QueryDSL 의존성 추가(#42)
    
    * feat: 잘못된 대화 태그 연관관계 수정(#42)
    
    대화 주제와 대화 태그간 관계는 다대일 관계이므로 수정
    
    * feat: QueryDSL 설정 구성(#42)
    
    JPAQueryFactory 빈 등록을 위한 설정 구성
    
    * feat: 연관 대화 주제 조회 로직 추가(#42)
    
    연관 대화 주제는 기준 주제의 조건들에 모두 부합해야 함.
    기준이 되는 조건들은 다음과 같다.
    - 동일한 대화 종류 태그
    - 동일한 대화 장소 태그
    - 기준 주제 대화 상황 문자열, 대화 상황에 포함
    - 기준 주제 대화 상대 성별과 일치
    - 기준 주제 대화 상대 연령대와 연령대 일치 혹은 포함되어야 함
    주제 조회시 제공되는 스크랩 여부는 서브 쿼리를 통해 제공.
    
    정렬 기준, 페이지네이션 도입 결정에 따라 추후 수정 예정
    
    * feat: 연관 대화 주제 조회 비즈니스 로직 추가(#42)
    
    * feat: 연관 대화 주제 조회 응답 DTO 정의(#42)
    
    * feat: 연관 대화 주제 조회 API 추가(#42)
    
    * chore: 테스트용 프로필 정의(#42)
    
    * test: 연관 대화 주제 조회, repository 단위 테스트 작성(#42)

---
+++ src/main/java/org/example/tokpik_be/talk_topic/repository/QueryDslTalkTopicRepository.java
@@ -0,0 +1,53 @@
+package org.example.tokpik_be.talk_topic.repository;
+
+import static org.example.tokpik_be.scrap.domain.QScrap.scrap;
+import static org.example.tokpik_be.scrap.domain.QScrapTopic.scrapTopic;
+import static org.example.tokpik_be.tag.domain.QPlaceTag.placeTag;
+import static org.example.tokpik_be.tag.domain.QTopicTag.topicTag;
+import static org.example.tokpik_be.talk_topic.domain.QTalkTopic.talkTopic;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.tokpik_be.talk_topic.domain.TalkTopic;
+import org.example.tokpik_be.talk_topic.dto.response.TalkTopicsRelatedResponse;
+import org.example.tokpik_be.talk_topic.dto.response.TalkTopicsRelatedResponse.TalkTopicRelatedResponse;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class QueryDslTalkTopicRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public TalkTopicsRelatedResponse findRelatedTalkTopics(long userId, TalkTopic baseTopic) {
+        List<TalkTopicRelatedResponse> relatedTopics = jpaQueryFactory.from(talkTopic)
+            .select(Projections.constructor(TalkTopicRelatedResponse.class,
+                talkTopic.id,
+                talkTopic.topicTag.content,
+                talkTopic.title,
+                JPAExpressions.selectOne()
+                    .from(scrapTopic)
+                    .innerJoin(scrapTopic.scrap, scrap)
+                    .where(scrapTopic.talkTopic.id.eq(talkTopic.id).and(scrap.user.id.eq(userId)))
+                    .exists()
+                    .as("scraped")
+            ))
+            .innerJoin(talkTopic.topicTag, topicTag)
+            .innerJoin(talkTopic.placeTag, placeTag)
+            .where(
+                talkTopic.topicTag.id.eq(baseTopic.getTopicTag().getId()),
+                talkTopic.placeTag.id.eq(baseTopic.getPlaceTag().getId()),
+                talkTopic.situation.contains(baseTopic.getSituation()),
+                talkTopic.partner.gender.eq(baseTopic.getPartner().getGender()),
+                talkTopic.partner.ageLowerBound.goe(baseTopic.getPartner().getAgeLowerBound()),
+                talkTopic.partner.ageUpperBound.loe(baseTopic.getPartner().getAgeUpperBound()),
+                talkTopic.id.ne(baseTopic.getId()))
+            .limit(10)
+            .fetch();
+
+        return new TalkTopicsRelatedResponse(relatedTopics);
+    }
+}
--- src/main/java/org/example/tokpik_be/talk_topic/service/TalkTopicQueryService.java
+++ src/main/java/org/example/tokpik_be/talk_topic/service/TalkTopicQueryService.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.TalkTopicException;
 import org.example.tokpik_be.talk_topic.domain.TalkTopic;
+import org.example.tokpik_be.talk_topic.dto.response.TalkTopicsRelatedResponse;
+import org.example.tokpik_be.talk_topic.repository.QueryDslTalkTopicRepository;
 import org.example.tokpik_be.talk_topic.repository.TalkTopicRepository;
+import org.example.tokpik_be.user.domain.User;
+import org.example.tokpik_be.user.service.UserQueryService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +18,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class TalkTopicQueryService {
 
     private final TalkTopicRepository talkTopicRepository;
+    private final QueryDslTalkTopicRepository queryDslTalkTopicRepository;
+
+    private final UserQueryService userQueryService;
 
     public TalkTopic findById(long topicId) {
 
         return talkTopicRepository.findById(topicId)
             .orElseThrow(() -> new GeneralException(TalkTopicException.TALK_TOPIC_NOT_FOUND));
+    }
+
+    public TalkTopicsRelatedResponse getRelatedTopics(long userId, long topicId) {
+        User user = userQueryService.findById(userId);
+        TalkTopic baseTopic = findById(topicId);
+
+        return queryDslTalkTopicRepository.findRelatedTalkTopics(user.getId(), baseTopic);
     }
 }
