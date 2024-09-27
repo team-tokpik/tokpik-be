513bc3b581dd46ccd84be347443c8c33f8d682f8
Minjae-An <mj3242@naver.com>
2024-09-27 21:16:48 +0900

    feat: 허용되지 않은 알림 삭제 시도 예외 정의(#39)

--- src/main/java/org/example/tokpik_be/exception/NotificationException.java
+++ src/main/java/org/example/tokpik_be/exception/NotificationException.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum NotificationException implements BaseException {
 
     NOTIFICATION_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 알림"),
-    INVALID_NOTIFICATION_INTERVAL(HttpStatus.BAD_REQUEST, "유효하지 않은 알림 간격");
+    INVALID_NOTIFICATION_INTERVAL(HttpStatus.BAD_REQUEST, "유효하지 않은 알림 간격"),
+    UNAUTHORIZED_NOTIFICATION_DELETION(HttpStatus.BAD_REQUEST, "자신의 알림만 삭제 가능");
 
     private final HttpStatus status;
     private final String message;
