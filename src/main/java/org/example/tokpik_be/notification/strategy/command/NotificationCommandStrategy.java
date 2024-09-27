fafc7fd2aca251e1865d025723264265d042a44e
Minjae-An <mj3242@naver.com>
2024-09-27 20:47:09 +0900

    feat: 코드 재설계를 위해 알림 연관 코드 전체 삭제(#39)

--- src/main/java/org/example/tokpik_be/notification/strategy/command/NotificationCommandStrategy.java
+++
@@ -1,8 +0,0 @@
-package org.example.tokpik_be.notification.strategy.command;
-
-public interface NotificationCommandStrategy {
-
-    void deleteById(long notificationId);
-    void deleteAllInBatch();
-    int getIntervalMinutes();
-}
