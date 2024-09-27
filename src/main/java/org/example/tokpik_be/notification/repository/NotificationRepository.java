ffa5bd365a73d46125e8d97192ae56967024d787
Minjae-An <mj3242@naver.com>
2024-09-27 21:17:56 +0900

    feat: 알림 repository 정의(#39)

---
+++ src/main/java/org/example/tokpik_be/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package org.example.tokpik_be.notification.repository;
+
+import org.example.tokpik_be.notification.domain.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
