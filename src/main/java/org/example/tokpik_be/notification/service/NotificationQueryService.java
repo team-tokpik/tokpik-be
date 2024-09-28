8618ca08c71721870cf7016291a0974543cafb86
Minjae-An <mj3242@naver.com>
2024-09-28 16:40:36 +0900

    feat: 알림 목록 조회 로직 추가(#39)

--- src/main/java/org/example/tokpik_be/notification/service/NotificationQueryService.java
+++ src/main/java/org/example/tokpik_be/notification/service/NotificationQueryService.java
@@ -1,10 +1,13 @@
 package org.example.tokpik_be.notification.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.NotificationException;
 import org.example.tokpik_be.notification.domain.Notification;
+import org.example.tokpik_be.notification.dto.response.NotificationsResponse;
 import org.example.tokpik_be.notification.repository.NotificationRepository;
+import org.example.tokpik_be.notification.repository.QueryDslNotificationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationQueryService {
 
     private final NotificationRepository notificationRepository;
+    private final QueryDslNotificationRepository queryDslNotificationRepository;
 
     public Notification findById(long notificationId) {
 
@@ -21,4 +25,11 @@ public class NotificationQueryService {
             .orElseThrow(() -> new GeneralException(NotificationException.NOTIFICATION_NOT_FOUND));
     }
 
+    public NotificationsResponse getNotifications(long userId, Long nextContentId) {
+        long nextNotificationId = Optional.ofNullable(nextContentId).orElse(Long.MAX_VALUE);
+        int pageSize = 10;
+
+        return queryDslNotificationRepository
+            .getNotifications(userId, nextNotificationId, pageSize);
+    }
 }
