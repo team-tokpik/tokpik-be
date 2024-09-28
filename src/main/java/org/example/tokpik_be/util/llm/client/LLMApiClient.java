3efdcc8d7b3117782997c1668c93949793f3f1f9
Minjae-An <mj3242@naver.com>
2024-09-28 17:26:12 +0900

    feat: 대화 주제 상세 생성 로직 정의(#44)

--- src/main/java/org/example/tokpik_be/util/llm/client/LLMApiClient.java
+++ src/main/java/org/example/tokpik_be/util/llm/client/LLMApiClient.java
@@ -1,9 +1,13 @@
 package org.example.tokpik_be.util.llm.client;
 
+import org.example.tokpik_be.util.llm.dto.request.LLMTalkTopicDetailRequest;
 import org.example.tokpik_be.util.llm.dto.request.LLMTalkTopicSearchRequest;
+import org.example.tokpik_be.util.llm.dto.response.LLMTalkTopicDetailResponse;
 import org.example.tokpik_be.util.llm.dto.response.LLMTalkTopicsResponse;
 
 public interface LLMApiClient {
 
     LLMTalkTopicsResponse searchTalkTopics(LLMTalkTopicSearchRequest request);
+
+    LLMTalkTopicDetailResponse generateTalkTopicDetail(LLMTalkTopicDetailRequest request);
 }
