43ae6bf8f6c176e78a5d51ed99444107ad483a46
Minjae-An <mj3242@naver.com>
2024-09-28 17:29:36 +0900

    feat: 대화 주제 조회 로직 추가(#44)

--- src/main/java/org/example/tokpik_be/talk_topic/service/TalkTopicQueryService.java
+++ src/main/java/org/example/tokpik_be/talk_topic/service/TalkTopicQueryService.java
@@ -1,14 +1,21 @@
 package org.example.tokpik_be.talk_topic.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.TalkTopicException;
+import org.example.tokpik_be.scrap.repository.QueryDslScrapRepository;
 import org.example.tokpik_be.talk_topic.domain.TalkTopic;
+import org.example.tokpik_be.talk_topic.dto.response.TalkTopicDetailResponse;
+import org.example.tokpik_be.talk_topic.dto.response.TalkTopicDetailResponse.TalkTopicDetailItemResponse;
 import org.example.tokpik_be.talk_topic.dto.response.TalkTopicsRelatedResponse;
 import org.example.tokpik_be.talk_topic.repository.QueryDslTalkTopicRepository;
 import org.example.tokpik_be.talk_topic.repository.TalkTopicRepository;
 import org.example.tokpik_be.user.domain.User;
 import org.example.tokpik_be.user.service.UserQueryService;
+import org.example.tokpik_be.util.llm.client.LLMApiClient;
+import org.example.tokpik_be.util.llm.dto.request.LLMTalkTopicDetailRequest;
+import org.example.tokpik_be.util.llm.dto.response.LLMTalkTopicDetailResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +26,10 @@ public class TalkTopicQueryService {
 
     private final TalkTopicRepository talkTopicRepository;
     private final QueryDslTalkTopicRepository queryDslTalkTopicRepository;
+    private final QueryDslScrapRepository queryDslScrapRepository;
 
     private final UserQueryService userQueryService;
+    private final LLMApiClient llmApiClient;
 
     public TalkTopic findById(long topicId) {
 
@@ -33,5 +42,21 @@ public class TalkTopicQueryService {
         TalkTopic baseTopic = findById(topicId);
 
         return queryDslTalkTopicRepository.findRelatedTalkTopics(user.getId(), baseTopic);
+    }
+
+    public TalkTopicDetailResponse getTalkTopicDetail(long userId, long topicId) {
+        User user = userQueryService.findById(userId);
+        TalkTopic talkTopic = findById(topicId);
+
+        LLMTalkTopicDetailRequest request = LLMTalkTopicDetailRequest.from(talkTopic);
+        LLMTalkTopicDetailResponse llmResponse = llmApiClient
+            .generateTalkTopicDetail(request);
+
+        List<TalkTopicDetailItemResponse> details = llmResponse.details().stream()
+            .map(item -> new TalkTopicDetailItemResponse(item.itemTitle(), item.itemContent()))
+            .toList();
+        boolean scraped = queryDslScrapRepository.checkTopicScrapedBy(user, talkTopic);
+
+        return new TalkTopicDetailResponse(details, scraped);
     }
 }
