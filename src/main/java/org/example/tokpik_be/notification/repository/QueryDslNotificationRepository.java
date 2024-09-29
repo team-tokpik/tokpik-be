e5f854e1908b72f36779318a6c7c7b7a74f695f0
Minjae-An <mj3242@naver.com>
2024-09-29 18:24:54 +0900

    feat: 알림 목록 조회 로직 수정(#39)
    로직은 다음 과정을 거친다.
    1. 대상 알림 ID 목록 조회, 이 때 pageSize + 1 만큼의 데이터 조회
    2. 조회된 ID 개수 바탕, 마지막 페이지 여부 도출
    3. 마지막 페이지 여부 바탕, 다음 커서 ID 도출
    4. 알림과 알림에 포함된 대화 주제들의 대화 종류 데이터 조회(응답 연관 데이터)
    5. 알림 ID별로 조회된 데이터 grouping
    6. 알림별 알림 대화 주제 ID 오름차순 정렬, 대화 주제 종류 ID/내용 collecting
    7. collecting한 데이터 바탕으로 순차적 응답 DTO 생성

--- src/main/java/org/example/tokpik_be/notification/repository/QueryDslNotificationRepository.java
+++ src/main/java/org/example/tokpik_be/notification/repository/QueryDslNotificationRepository.java
@@ -3,13 +3,16 @@ package org.example.tokpik_be.notification.repository;
 import static org.example.tokpik_be.notification.domain.QNotification.notification;
 import static org.example.tokpik_be.notification.domain.QNotificationTalkTopic.notificationTalkTopic;
 import static org.example.tokpik_be.scrap.domain.QScrap.scrap;
+import static org.example.tokpik_be.tag.domain.QTopicTag.topicTag;
 import static org.example.tokpik_be.talk_topic.domain.QTalkTopic.talkTopic;
 
 import com.querydsl.core.Tuple;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.notification.dto.response.NotificationsResponse;
@@ -23,71 +26,89 @@ public class QueryDslNotificationRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public NotificationsResponse getNotifications(long userId,
-        long nextNotificationId,
-        int pageSize) {
-        // 서브쿼리 : 각 알림에 대해 최대 3개의 대화 주제 선택
-        JPQLQuery<Tuple> topicSubQuery = JPAExpressions.select(notificationTalkTopic.notificationId,
-                talkTopic.id,
-                talkTopic.title)
-            .from(notificationTalkTopic)
-            .join(talkTopic).on(notificationTalkTopic.talkTopic.id.eq(talkTopic.id))
-            .where(notificationTalkTopic.notificationId.eq(notification.id))
-            .orderBy(notificationTalkTopic.id.asc())
-            .limit(3);
+    public NotificationsResponse getNotifications(long userId, Long cursorId, int pageSize) {
+        // 사용자 보유 알림 확인 where 조건 정의
+        BooleanExpression usersNotificationCondition = notification.user.id.eq(userId);
 
+        // 첫 페이지 여부 도출
+        boolean first = Optional.ofNullable(cursorId).isEmpty();
+
+        // 다음 페이지 존재 확인 where 조건 정의
+        BooleanExpression nextPageCondition = Optional.ofNullable(cursorId)
+            .map(id -> notification.id.gt(cursorId))
+            .orElse(null);
+
+        // 대상 알림 ID 목록 조회
+        List<Long> notificationIds = queryFactory.select(notification.id)
+            .from(notification)
+            .where(usersNotificationCondition.and(nextPageCondition))
+            .limit(pageSize + 1)
+            .fetch();
+
+        // 마지막 페이지 여부 도출
+        boolean last = notificationIds.size() <= pageSize;
+        Long nextCursorId = null;
+
+        // pageSize + 1개 조회시 마지막 항목 제거, 다음 커서 ID 설정
+        if (!last) {
+            nextCursorId = notificationIds.get(notificationIds.size() - 1);
+            notificationIds = notificationIds.subList(0, pageSize + 1);
+        }
+
+        // 알림과 알림 포함된 대화 주제들의 대화 종류 데이터 조회
         List<Tuple> results = queryFactory.from(notification)
             .select(notification.id,
                 notification.noticeDate,
                 notification.startTime,
                 notification.endTime,
                 notification.intervalMinutes,
+                notificationTalkTopic.id,
                 scrap.title,
-                JPAExpressions.select(notificationTalkTopic.count())
-                    .from(notificationTalkTopic)
-                    .where(notificationTalkTopic.notificationId.eq(notification.id)),
-                topicSubQuery.select(talkTopic.id),
-                topicSubQuery.select(talkTopic.title))
-            .join(scrap).on(notification.scrap.id.eq(scrap.id))
-            .where(notification.user.id.eq(userId)
-                .and(notification.deleted.isFalse())
-                .and(notification.id.lt(nextNotificationId)))
-            .orderBy(notification.noticeDate.asc(), notification.startTime.asc())
-            .limit(pageSize + 1)
+                topicTag.id,
+                topicTag.content)
+            .join(notification.notificationTalkTopics, notificationTalkTopic)
+            .join(notification.scrap, scrap)
+            .join(notificationTalkTopic.talkTopic, talkTopic)
+            .join(talkTopic.topicTag, topicTag)
+            .where(usersNotificationCondition.and(notification.id.in(notificationIds)))
             .fetch();
 
-        List<NotificationResponse> contents = results.stream()
-            .collect(Collectors.groupingBy(tuple -> tuple.get(notification.id),
-                Collectors.collectingAndThen(
-                    Collectors.toList(),
-                    tuples -> {
-                        Tuple firstTuple = tuples.get(0);
-                        List<NotificationTalkTopicTypeResponse> topicTypes = tuples.stream()
-                            .map(t -> new NotificationTalkTopicTypeResponse(
-                                t.get(7, Long.class),
-                                t.get(8, String.class)
-                            ))
-                            .toList();
+        // 알림 ID별로 조회 데이터들 grouping
+        Map<Long, List<Tuple>> groupByNotificationId = results.stream()
+            .collect(Collectors.groupingBy(result -> result.get(notification.id)));
 
-                        return new NotificationResponse(firstTuple.get(notification.id),
-                            firstTuple.get(notification.noticeDate),
-                            firstTuple.get(notification.startTime),
-                            firstTuple.get(notification.endTime),
-                            firstTuple.get(notification.intervalMinutes),
-                            firstTuple.get(scrap.title),
-                            firstTuple.get(6, Long.class),
-                            topicTypes
-                        );
-                    })))
-            .values()
-            .stream()
-            .limit(pageSize)
+        // group 바탕으로 알림별 알림 대화 주제 ID(알림 지정 순서)에 따라 정렬, 첫 세 알림 대화 주제 선정, 응답 DTO로 매핑
+        List<NotificationResponse> contents = groupByNotificationId.entrySet().stream()
+            .map(entry -> {
+                long notificationId = entry.getKey();
+                List<Tuple> tuples = entry.getValue();
+
+                long notificationTopicTotal = tuples.size();
+
+                List<NotificationTalkTopicTypeResponse> talkTopicTypeResponses = tuples
+                    .stream()
+                    .sorted(Comparator.comparing(r -> r.get(notificationTalkTopic.id)))
+                    .toList().subList(0, 3)
+                    .stream()
+                    .map(r -> new NotificationTalkTopicTypeResponse(r.get(topicTag.id),
+                        r.get(topicTag.content)))
+                    .toList();
+
+                Tuple tuple = tuples.get(0);
+
+                return new NotificationResponse(notificationId,
+                    tuple.get(notification.noticeDate),
+                    tuple.get(notification.startTime),
+                    tuple.get(notification.endTime),
+                    tuple.get(notification.intervalMinutes),
+                    tuple.get(scrap.title),
+                    notificationTopicTotal,
+                    talkTopicTypeResponses);
+            })
+            .sorted(Comparator.comparingLong(NotificationResponse::notificationId))
             .toList();
 
-        boolean hasNext = results.size() > pageSize;
-        Long nextContentId = hasNext ? contents.get(contents.size() - 1).notificationId() : null;
-
-        return new NotificationsResponse(contents, nextContentId, nextNotificationId == 1,
-            !hasNext);
+        return new NotificationsResponse(contents, nextCursorId, first, last);
     }
 }
+
