e532b2f8da15739df2af3d86f7af3a29fff2af84
Minjae-An <mj3242@naver.com>
2024-09-29 18:28:05 +0900

    feat: 검증 조건 추가, 명세 수정(#39)
    - 조건이 존재할 경우, 유효한 문자열 여부 확인토록 검증 추가
    - array 타입 필드들, 명세 올바르게 변경

--- src/main/java/org/example/tokpik_be/talk_topic/dto/request/TalkTopicSearchRequest.java
+++ src/main/java/org/example/tokpik_be/talk_topic/dto/request/TalkTopicSearchRequest.java
@@ -1,20 +1,21 @@
 package org.example.tokpik_be.talk_topic.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
 public record TalkTopicSearchRequest(
     @Schema(type = "boolean", description = "필터(검색 조건) 사용 여부", example = "true")
     boolean includeFilterCondition,
 
-    @Schema(type = "string", description = "대화목적", example = "친목")
-    List<String> talkPurposes,
+    @Schema(type = "array", description = "대화목적", example = "친목")
+    List<@NotBlank(message = "대화목적은 유효한 문자열만 가능") String> talkPurposes,
 
-    @Schema(type = "string", description = "대화상황", example = "첫만남")
-    List<String> talkSituations,
+    @Schema(type = "array", description = "대화상황", example = "첫만남")
+    List<@NotBlank(message = "대화상황은 유효한 문자열만 가능") String> talkSituations,
 
-    @Schema(type = "string", description = "대화분위기", example = "유익한 분위기")
-    List<String> talkMoods,
+    @Schema(type = "array", description = "대화분위기", example = "유익한 분위기")
+    List<@NotBlank(message = "대화분위기는 유효한 문자열만 가능") String> talkMoods,
 
     @Schema(type = "boolean", description = "대화상대 성별, 남=true/여=false", example = "true")
     Boolean talkPartnerGender,
