6988c1f226ff1bb1902e7c7c1d455a148c6280c7
Minjae-An <mj3242@naver.com>
2024-09-30 21:05:54 +0900

    feat: 알림 생성 비즈니스 로직 추가(#57)
    로직은 다음 과정을 거쳐 이뤄진다.
    1. 사용자, 스크랩 조회
    2. 요청한 사용자 보유 스크랩인 지 검증
    3. 요청된 알림 간격 검증(5~20분 간격인 지)
    4. 알림 생성 및 저장
    5. 알림 대화 주제 ID들 스크랩 포함 여부, 개수 검증
    6. 알림 대화 주제 생성 및 저장, 알림-알림 대화 주제 연관관계 형성

--- src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
+++ src/main/java/org/example/tokpik_be/notification/service/NotificationCommandService.java
@@ -4,10 +4,17 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.exception.GeneralException;
 import org.example.tokpik_be.exception.NotificationException;
+import org.example.tokpik_be.exception.ScrapException;
 import org.example.tokpik_be.notification.domain.Notification;
 import org.example.tokpik_be.notification.domain.NotificationTalkTopic;
+import org.example.tokpik_be.notification.dto.request.NotificationCreateRequest;
 import org.example.tokpik_be.notification.repository.NotificationRepository;
 import org.example.tokpik_be.notification.repository.NotificationTalkTopicRepository;
+import org.example.tokpik_be.scrap.domain.Scrap;
+import org.example.tokpik_be.scrap.domain.ScrapTopic;
+import org.example.tokpik_be.scrap.repository.ScrapRepository;
+import org.example.tokpik_be.talk_topic.domain.TalkTopic;
+import org.example.tokpik_be.talk_topic.repository.TalkTopicRepository;
 import org.example.tokpik_be.user.domain.User;
 import org.example.tokpik_be.user.service.UserQueryService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,11 +26,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class NotificationCommandService {
 
+    private final List<Integer> notificationIntervalMinutes = List.of(5, 10, 15, 20);
+
     private final NotificationRepository notificationRepository;
     private final NotificationTalkTopicRepository notificationTalkTopicRepository;
+    private final ScrapRepository scrapRepository;
+    private final TalkTopicRepository talkTopicRepository;
 
     private final NotificationQueryService notificationQueryService;
     private final UserQueryService userQueryService;
+
 
     public void deleteNotification(long userId, long notificationId) {
         Notification notification = notificationQueryService.findById(notificationId);
@@ -48,5 +60,55 @@ public class NotificationCommandService {
             .map(NotificationTalkTopic::getId)
             .toList();
         notificationTalkTopicRepository.deleteAllByIdInBatch(notificationTalkTopicIds);
+    }
+
+    public void createNotification(long userId, NotificationCreateRequest request) {
+        User user = userQueryService.findById(userId);
+
+        Scrap scrap = scrapRepository.findById(request.scrapId())
+            .orElseThrow(() -> new GeneralException(ScrapException.SCRAP_NOT_FOUND));
+
+        // 요청한 사용자의 스크랩인 지 검증
+        if (!scrap.getUser().equals(user)) {
+            throw new GeneralException(ScrapException.UNAUTHORIZED_SCRAP_ACCESS);
+        }
+
+        // 요청된 알림 간격 검증
+        int intervalMinutes = request.notificationIntervalMinutes();
+        if (notificationIntervalMinutes.contains(intervalMinutes)) {
+            throw new GeneralException(NotificationException.INVALID_NOTIFICATION_INTERVAL);
+        }
+
+        // 알림 생성 및 저장
+        Notification notification = new Notification(request.noticeDate(),
+            request.notificationStartTime(),
+            request.notificationEndTime(),
+            intervalMinutes,
+            user,
+            scrap);
+        notificationRepository.save(notification);
+
+        // 알림 대화 주제 ID들 지정한 스크랩 포함 여부, 개수 검증
+        List<Long> notificationTalkTopicIds =
+            request.notificationTalkTopicIds();
+        List<Long> talkTopicIds = scrap.getScrapTopics().stream()
+            .map(ScrapTopic::getTalkTopic)
+            .map(TalkTopic::getId)
+            .toList();
+        List<Long> filteredIds = notificationTalkTopicIds.stream()
+            .filter(talkTopicIds::contains)
+            .toList();
+        if (filteredIds.size() != notificationTalkTopicIds.size()) {
+            throw new GeneralException(NotificationException.CAN_NOTICE_TALK_TOPICS_IN_SCRAP);
+        }
+
+        // 알림 대화 주제 생성 및 저장, 연관관계 설정
+        long notificationId = notification.getId();
+        List<TalkTopic> talkTopics = talkTopicRepository.findAllById(notificationTalkTopicIds);
+        List<NotificationTalkTopic> notificationTalkTopics = talkTopics.stream()
+            .map(talkTopic -> new NotificationTalkTopic(notificationId, talkTopic))
+            .toList();
+        notificationTalkTopicRepository.saveAll(notificationTalkTopics);
+        notification.addNotificationTalkTopics(notificationTalkTopics);
     }
 }
