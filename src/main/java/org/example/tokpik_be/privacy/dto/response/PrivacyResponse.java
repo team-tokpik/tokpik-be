4134a475b564488a81b906635bc4c9cf53a1eeb7
hyunji <phj9573@naver.com>
2024-10-01 16:37:48 +0900

    feat: 개인정보정책 응답 DTO 정의(#63)

---
+++ src/main/java/org/example/tokpik_be/privacy/dto/response/PrivacyResponse.java
@@ -0,0 +1,12 @@
+package org.example.tokpik_be.privacy.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PrivacyResponse (
+    @Schema(type = "string", description = "개인정보정책 제목", example = "데이터 분석")
+    String title,
+
+    @Schema(type = "string", description = "개인정보정책 내용", example = "일부 모바일 앱에서 사용자의..")
+    String content
+){
+}
