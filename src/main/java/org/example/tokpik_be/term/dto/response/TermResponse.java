9b341983c32b4e65c0ef3976d07a499cff3ae214
hyunji <phj9573@naver.com>
2024-10-01 16:25:17 +0900

    feat: 이용약관 응답 DTO 수정(#63)

--- src/main/java/org/example/tokpik_be/term/dto/response/TermResponse.java
+++ src/main/java/org/example/tokpik_be/term/dto/response/TermResponse.java
@@ -6,17 +6,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record TermResponse(
 
-    @Schema(type = "string", description = "이용약관", example = "콘텐츠에 대한 라이선스")
+    @Schema(type = "string", description = "이용약관 전체 제목", example = "콘텐츠에 대한 라이선스")
     String title,
     List<TermsSection> sections
 ) {
     public record TermsSection(
-
-        @Schema(type = "number", description = "메인 카테고리", example = "1")
+        @Schema(type = "string", description = "메인 카테고리", example = "1")
         String mainCategory,
-        @Schema(type = "number", description = "서브 카테고리", example = "2")
+        @Schema(type = "string", description = "서브 카테고리", example = "1")
         String subCategory,
-        @Schema(type = "string", description = "콘텐츠", example = "이용약관 내용..")
+        @Schema(type = "string", description = "콘텐츠 제목", example = "업로드된 콘텐츠")
+        String contentTitle,
+        @Schema(type = "string", description = "콘텐츠 내용", example = "이 콘텐츠 에서는..")
         String content
     ) {}
 }
