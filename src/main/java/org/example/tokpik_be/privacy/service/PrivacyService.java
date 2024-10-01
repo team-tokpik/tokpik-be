9c4a7cc2ba29b2e5ceba17396c6f713993550c14
hyunji <phj9573@naver.com>
2024-10-01 16:50:16 +0900

    feat: 개인정보정책 조회 로직 구현(#63)

--- src/main/java/org/example/tokpik_be/privacy/service/PrivacyService.java
+++ src/main/java/org/example/tokpik_be/privacy/service/PrivacyService.java
@@ -1,6 +1,12 @@
 package org.example.tokpik_be.privacy.service;
 
+import java.util.List;
+
+import org.example.tokpik_be.privacy.domain.Privacy;
+import org.example.tokpik_be.privacy.dto.response.PrivacyResponse;
+import org.example.tokpik_be.privacy.repository.PrivacyRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,5 +14,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PrivacyService {
 
+    private final PrivacyRepository privacyRepository;
 
+    @Transactional(readOnly = true)
+    public List<PrivacyResponse> getAllPrivacy() {
+        List<Privacy> privacies = privacyRepository.findAll();
+        return privacies.stream()
+            .map(privacy -> new PrivacyResponse(privacy.getTitle(), privacy.getContent()))
+            .toList();
+    }
 }
