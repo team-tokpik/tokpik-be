0fdacd231993ad45f87f3bec21c11748a465ea2a
hyunji <phj9573@naver.com>
2024-10-01 16:43:27 +0900

    feat: policy 엔티티 정의(#63)

---
+++ src/main/java/org/example/tokpik_be/privacy/domain/Privacy.java
@@ -0,0 +1,33 @@
+package org.example.tokpik_be.privacy.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "privacys")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Privacy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    public Privacy(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
