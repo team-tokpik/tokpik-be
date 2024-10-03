94d915b1187d7d49cbc5a92aff06621dd1807642
Minjae-An <mj3242@naver.com>
2024-10-03 19:53:02 +0900

    test: 서비스 단위 테스트 환경 구성(#67)
    - 서비스 단위 테스트 사용되는 공통 환경 정의
    - 추상 클래스로 선언해 상속 받아 이용할 수 있도록 구성

---
+++ src/test/java/org/example/tokpik_be/support/ServiceTestSupport.java
@@ -0,0 +1,45 @@
+package org.example.tokpik_be.support;
+
+import jakarta.persistence.EntityManager;
+import org.example.tokpik_be.tag.repository.PlaceTagRepository;
+import org.example.tokpik_be.tag.repository.TopicTagRepository;
+import org.example.tokpik_be.tag.repository.UserPlaceTagRepository;
+import org.example.tokpik_be.tag.repository.UserTopicTagRepository;
+import org.example.tokpik_be.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
+public abstract class ServiceTestSupport {
+
+    @Autowired
+    protected EntityManager em;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected TopicTagRepository topicTagRepository;
+
+    @Autowired
+    protected PlaceTagRepository placeTagRepository;
+
+    @Autowired
+    protected UserTopicTagRepository userTopicTagRepository;
+
+    @Autowired
+    protected UserPlaceTagRepository userPlaceTagRepository;
+
+    @AfterEach
+    void tearDown() {
+        topicTagRepository.deleteAllInBatch();
+        placeTagRepository.deleteAllInBatch();
+
+        userTopicTagRepository.deleteAllInBatch();
+        userPlaceTagRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+    }
+}
