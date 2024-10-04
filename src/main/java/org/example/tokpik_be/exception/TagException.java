70e35264422cc490311beed1a50d74310aa177de
hyunji <phj9573@naver.com>
2024-10-04 12:53:24 +0900

    feat: 중복된 태그 관련 예외 정의(#72)

--- src/main/java/org/example/tokpik_be/exception/TagException.java
+++ src/main/java/org/example/tokpik_be/exception/TagException.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum TagException implements BaseException {
 
     TAG_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 태그"),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "1개 이상의 태그 선택 필요");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "1개 이상의 태그 선택 필요"),
+    DUPLICATE_TAGS(HttpStatus.BAD_REQUEST, "중복된 태그 요청 존재");
 
     private final HttpStatus status;
     private final String message;
