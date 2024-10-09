4988a4c09449622dce26fefb00311a08462f25c3
hyunji <phj9573@naver.com>
2024-10-09 11:37:17 +0900

    test: 스크랩 조회 API 단위테스트 작성(#80)

--- src/test/java/org/example/tokpik_be/scrap/controller/ScrapControllerTest.java
+++ src/test/java/org/example/tokpik_be/scrap/controller/ScrapControllerTest.java
@@ -7,8 +7,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 
 import org.example.tokpik_be.exception.GeneralException;
+import org.example.tokpik_be.exception.ScrapException;
 import org.example.tokpik_be.exception.UserException;
 import org.example.tokpik_be.scrap.dto.response.ScrapListResponse;
+import org.example.tokpik_be.scrap.dto.response.ScrapResponse;
 import org.example.tokpik_be.scrap.service.ScrapService;
 import org.example.tokpik_be.support.ControllerTestSupport;
 import org.junit.jupiter.api.DisplayName;
@@ -32,6 +34,10 @@ public class ScrapControllerTest extends ControllerTestSupport {
     }
 
     private final long userId = 1L;
+    private final long nextCursorId = 0L;
+    private final int size = 10;
+    private final long scrapId = 10L;
+
 
     @Nested
     @DisplayName("스크랩 리스트 목록 조회 시 ")
@@ -92,6 +98,65 @@ public class ScrapControllerTest extends ControllerTestSupport {
             // Then
             resultActions.andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value(UserException.USER_NOT_FOUND.getMessage()));
+        }
+    }
+
+    @Nested
+    @DisplayName("스크랩에 포함된 대화 주제 조회 시 ")
+    class getScrapTopicsTest{
+
+        @Test
+        @DisplayName("성공한다.")
+        void success() throws Exception {
+
+            // Given
+            ScrapResponse response = new ScrapResponse(
+                List.of(
+                    new ScrapResponse.ScrapTopicResponse(1L, 1L, "첫만남에 스몰토크", "아이스브레이킹", true),
+                    new ScrapResponse.ScrapTopicResponse(2L, 4L, "재밌는 이야기", "유머와 웃음", false)
+                ),
+                2L, true, false
+            );
+            given(scrapService.getScrapTopics(scrapId, nextCursorId, size)).willReturn(response);
+
+            // When
+            ResultActions resultActions = mockMvc.perform(get("/users/scraps/{scrapId}/topics", scrapId)
+                .param("nextCursorId", String.valueOf(nextCursorId))
+                .param("size", String.valueOf(size)));
+
+            // Then
+            resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.contents.[0].scrapTopicId").value(1L))
+                .andExpect(jsonPath("$.contents.[0].topicId").value(1L))
+                .andExpect(jsonPath("$.contents.[0].topicTitle").value("첫만남에 스몰토크"))
+                .andExpect(jsonPath("$.contents.[0].topicType").value("아이스브레이킹"))
+                .andExpect(jsonPath("$.contents.[0].scraped").value(true))
+                .andExpect(jsonPath("$.contents.[1].scrapTopicId").value(2L))
+                .andExpect(jsonPath("$.contents.[1].topicId").value(4L))
+                .andExpect(jsonPath("$.contents.[1].topicTitle").value("재밌는 이야기"))
+                .andExpect(jsonPath("$.contents.[1].topicType").value("유머와 웃음"))
+                .andExpect(jsonPath("$.contents.[1].scraped").value(false))
+                .andExpect(jsonPath("$.nextCursorId").value(2L))
+                .andExpect(jsonPath("$.first").value(true))
+                .andExpect(jsonPath("$.last").value(false));
+        }
+
+        @Test
+        @DisplayName("스크랩이 존재하지 않으면 예외가 발생한다.")
+        void scrapNotFound() throws Exception {
+            // Given
+            given(scrapService.getScrapTopics(scrapId, nextCursorId, size))
+                .willThrow(new GeneralException(ScrapException.SCRAP_NOT_FOUND));
+
+            // When
+            ResultActions resultActions = mockMvc.perform(get("/users/scraps/{scrapId}/topics", scrapId)
+                .param("nextCursorId", String.valueOf(nextCursorId))
+                .param("size", String.valueOf(size)));
+
+            // Then
+            resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ScrapException.SCRAP_NOT_FOUND.getMessage()));
         }
     }
 
