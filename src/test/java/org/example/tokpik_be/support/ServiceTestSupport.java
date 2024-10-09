4132dd9a77409824f781d44f4251d8cb537c786b
hyunji <phj9573@naver.com>
2024-10-09 13:44:10 +0900

    test: 스크랩 리스트 조회 비즈니스 로직 단위테스트 작성(#80)

--- src/test/java/org/example/tokpik_be/support/ServiceTestSupport.java
+++ src/test/java/org/example/tokpik_be/support/ServiceTestSupport.java
@@ -1,9 +1,12 @@
 package org.example.tokpik_be.support;
 
+import org.example.tokpik_be.scrap.repository.ScrapRepository;
+import org.example.tokpik_be.scrap.repository.ScrapTopicRepository;
 import org.example.tokpik_be.tag.repository.PlaceTagRepository;
 import org.example.tokpik_be.tag.repository.TopicTagRepository;
 import org.example.tokpik_be.tag.repository.UserPlaceTagRepository;
 import org.example.tokpik_be.tag.repository.UserTopicTagRepository;
+import org.example.tokpik_be.talk_topic.repository.TalkTopicRepository;
 import org.example.tokpik_be.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +37,21 @@ public abstract class ServiceTestSupport {
     @Autowired
     protected UserPlaceTagRepository userPlaceTagRepository;
 
+    @Autowired
+    protected ScrapRepository scrapRepository;
+
+    @Autowired
+    protected ScrapTopicRepository scrapTopicRepository;
+
+    @Autowired
+    protected TalkTopicRepository talkTopicRepository;
+
     @AfterEach
     void tearDown() {
+        scrapTopicRepository.deleteAllInBatch();
+        scrapRepository.deleteAllInBatch();
+        talkTopicRepository.deleteAllInBatch();
+
         userTopicTagRepository.deleteAllInBatch();
         userPlaceTagRepository.deleteAllInBatch();
 
