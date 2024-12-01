ec6027f5b5ba007d0bde1d395e0599904ad077a0
Minjae-An <mj3242@naver.com>
2024-12-01 17:53:23 +0900

    feat: 코드 정렬 수정(#114)

--- src/main/java/org/example/tokpik_be/login/controller/LoginController.java
+++ src/main/java/org/example/tokpik_be/login/controller/LoginController.java
@@ -1,10 +1,5 @@
 package org.example.tokpik_be.login.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.example.tokpik_be.login.dto.request.AccessTokenRefreshRequest;
 import org.example.tokpik_be.login.dto.request.LoginByKakaoRequest;
 import org.example.tokpik_be.login.dto.response.AccessTokenRefreshResponse;
@@ -14,6 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Login API", description = "로그인 관련 API")
 @RestController
@@ -25,8 +26,7 @@ public class LoginController {
     @Operation(summary = "카카오 소셜 로그인", description = "카카오 인카 코드 바탕 소셜 로그인 수행")
     @ApiResponse(responseCode = "200", description = "카카오 소셜 로그인 성공")
     @PostMapping("/login/kakao")
-    public ResponseEntity<LoginResponse> kakaoLogin(
-        @RequestBody @Valid LoginByKakaoRequest request) {
+    public ResponseEntity<LoginResponse> kakaoLogin(@RequestBody @Valid LoginByKakaoRequest request) {
 
         LoginResponse response = loginCommandService.kakaoLogin(request);
 
